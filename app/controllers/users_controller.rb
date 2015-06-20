ad3648ca893f5fa4ffc9926950c435029af58701
marino-k <securio12@gmail.com>
2015-06-21 01:14:34 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,11 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+
   private
 
     def user_params
